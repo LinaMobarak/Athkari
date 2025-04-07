061f2323aeec9a30dc6d5be84a6bf598682fa251
Salem <sloom99q@Gmail.com>
2025-04-07 10:21:39 +0400

    Uploading all files

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -3,23 +3,28 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
+ 
+
   const theme = useColorScheme() ?? 'light';
 
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <TouchableOpacity
         style={styles.heading}
-        onPress={() => setIsOpen((value) => !value)}
+        onPress={() => {
+          setIsOpen((value) => !value)
+        }}
         activeOpacity={0.8}>
-        <IconSymbol
-          name="chevron.right"
-          size={18}
+        <MaterialIcons
+          name="chevron-left" 
+          size={30}
           weight="medium"
           color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
           style={{ transform: [{ rotate: isOpen ? '90deg' : '0deg' }] }}
@@ -27,19 +32,43 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
 
         <ThemedText type="defaultSemiBold">{title}</ThemedText>
       </TouchableOpacity>
-      {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
+      {isOpen && 
+      
+        <ThemedView style={styles.content}>{children}
+        
+        </ThemedView>
+        
+      
+      }
+
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  heading: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 6,
-  },
-  content: {
-    marginTop: 6,
-    marginLeft: 24,
-  },
+    heading: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 6,
+        padding: 20,
+        direction: 'rtl',
+        
+      },
+      content: {
+        marginTop: 6,
+        position: 'relative',
+        justifyContent: 'space-around'
+        
+      },
+      container: {
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        marginTop: 10,
+        fontSize: 20,
+        borderRadius: 10,
+        width: '90%',
+        borderColor: 'rgb(60, 115, 128)',
+        borderWidth: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      }
 });
