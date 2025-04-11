32a79e722408caed39275c3b6f38a1ca60e154fb
Salem <sloom99q@Gmail.com>
2025-04-11 22:53:35 +0400

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -63,7 +63,7 @@ export default function HomeScreen() {
           headerTitle: "أذكار المسلم",
         }}
       />
-{/* debugging */}
+
       <ParallaxScrollView
         headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
         headerImage={
